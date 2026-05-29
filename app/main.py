2684b1b0d255b7c12efe68ec8cedc53dbc823724
Sanjeev Uppu <uppusanjeev2005@gmail.com>
2026-05-29 11:50:59 +0530

    updated main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="SHMS Backend Sprint 1")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
